3a5304382e4dcee303894beaa78ea7cc242ffcdb
helloWon <leeseongwon13@gmail.com>
2022-02-12 03:00:41 +0900

    [함수형 인터페이스와 람다] 자바에서 제공하는 함수형 인터페이스

--- src/main/java/me/java8to11/Foo.java
+++ src/main/java/me/java8to11/Foo.java
@@ -1,16 +1,38 @@
 package me.java8to11;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
 public class Foo {
     public static void main(String[] args) {
+        Function<Integer, Integer> plus10 = (i) -> i + 10;
+        Function<Integer, Integer> multiply2 = (i) -> i * 2;
+        System.out.println(plus10.apply(1));
+        System.out.println(multiply2.apply(1));
 
-        // 익명 내부 클래스 anonymous inner class & lambda 표현식
-        RunSomething runSomething = (number) -> {
-            return number + 10;
-        };
+        // higher order function
+        Function<Integer, Integer> multiply2AndPlus10 = plus10.compose(multiply2);
+        System.out.println(multiply2AndPlus10.apply(2));
 
-        // 같은 값을 넣었을 때 항상 같은 결과 -> 순수한 함수, 상태값 의존 X
-        System.out.println(runSomething.doIt(1));
-        System.out.println(runSomething.doIt(1));
-        System.out.println(runSomething.doIt(1));
+        System.out.println(plus10.andThen(multiply2).apply(2));
+
+        // consumer
+        Consumer<Integer> printT = (i) -> System.out.println(i);
+        printT.accept(10);
+
+        // supplier: return 값 주는거
+        Supplier<Integer> get10 = () -> 10;
+        System.out.println(get10);
+
+        // predicate: 어떤 인자 하나 받아서 bool return
+        Predicate<String> startsWithSeongwon = (s) -> s.startsWith("seongwon");
+        Predicate<Integer> isEven = (i) -> i % 2 == 0;
+
+        // unaryOperator: 입출력 타입이 같은 경우
+        UnaryOperator<Integer> multiply2_2 = (i) -> i * 2;
+
     }
 }
