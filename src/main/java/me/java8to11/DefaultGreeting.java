aeea2168726830fe1f2eeaa0c8cbe5a240e83ee6
helloWon <leeseongwon13@gmail.com>
2022-02-13 12:35:47 +0900

    [인터페이스의 변화] 자바 8 API의 기본 메소드와 스태틱 메소드

--- src/main/java/me/java8to11/DefaultGreeting.java
+++ src/main/java/me/java8to11/DefaultGreeting.java
@@ -1,5 +1,10 @@
 package me.java8to11;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Spliterator;
+
 public class DefaultGreeting implements GreetingInterface, Bar {
 
     @Override
@@ -18,5 +23,38 @@ public class DefaultGreeting implements GreetingInterface, Bar {
         greetingInterface.printName();
 
         GreetingInterface.printAnything(); // static method
+
+        List<String> name = new ArrayList<>();
+        name.add("seongwon");
+        name.add("one");
+        name.add("two");
+        name.add("three");
+
+        // java8 - forEach
+        name.forEach(System.out::println);
+
+        // java8 - spliterator
+        Spliterator<String> spliterator = name.spliterator();
+        Spliterator<String> spliterator1 = spliterator.trySplit();
+        while (spliterator.tryAdvance(System.out::println))
+            ;
+        System.out.println("=================");
+        while (spliterator1.tryAdvance(System.out::println))
+            ;
+
+        // java8 - stream
+        long result = name.stream().map(String::toUpperCase).filter(s -> s.startsWith("S")).count();
+        System.out.println(result);
+
+        // java8 - removeif
+        name.removeIf(s -> s.startsWith("s"));
+        name.forEach(System.out::println);
+
+        // java8 - comparator
+        System.out.println("=================");
+        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+        name.sort(compareToIgnoreCase.reversed()); // thenComparing()
+        name.forEach(System.out::println);
+
     }
 }
